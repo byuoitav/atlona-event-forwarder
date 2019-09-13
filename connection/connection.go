9984939722f33755b52ee0d34d17a95a5141fd62
werndog <agwerner@byu.edu>
2019-09-13 09:25:33 -0600

    it should be almost done

--- connection/connection.go
+++ connection/connection.go
@@ -1,16 +1,16 @@
 package connection
 
 import (
-	"context"
-	"io/ioutil"
+	"bytes"
+	"encoding/json"
+	"net"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/byuoitav/common/db/couch"
 	"github.com/byuoitav/common/log"
-	"github.com/byuoitav/common/structs"
+	"github.com/byuoitav/common/v2/events"
 	"github.com/gorilla/websocket"
 )
 
@@ -20,100 +20,22 @@ var (
 	password = os.Getenv("DB_PASSWORD")
 )
 
-//Conns is a map of ID to websocket
-var Conns map[string]*websocket.Conn
-
-//MonitorAGWList monitors the list and updates it when a new gateway has been added
-func MonitorAGWList(currentAGWList []structs.Device) {
-	Conns = make(map[string]*websocket.Conn)
-	for _, i := range currentAGWList {
-		dialer := &websocket.Dialer{}
-		address := "ws://"
-		address += i.Address
-		address += "/ws"
-		ws, resp, err := dialer.DialContext(context.TODO(), address, nil)
-		if err != nil {
-			log.L.Fatalf("unable to open websocket: %s", err)
-		}
-		defer resp.Body.Close()
-
-		bytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.L.Fatalf("unable to read bytes: %s", bytes)
-		}
-
-		log.L.Debugf("response from opening websocket: %s", bytes)
-		Conns[i.Name] = ws
-	}
-	for {
-		//wait some time
-		log.L.Debugf("Waiting to check for list changes")
-		time.Sleep(5 * time.Minute)
-
-		log.L.Debugf("Checking AGWList for changes")
-
-		db := couch.NewDB(address, username, password)
-
-		agwList, err := db.GetDevicesByType("AtlonaGateway")
-		if err != nil {
-			log.L.Debugf("there was an issue getting the AGWList: %v", err)
-		}
-
-		//check to see if the length is different
-		if len(agwList) != len(currentAGWList) {
-			newList := make([]structs.Device, len(agwList))
-			copy(newList, agwList)
-			log.L.Debugf("comparing the list with the map to find the new one")
-
-			for i := 0; i < len(newList); i++ {
-				//for each object in newList check to see if it exists in the map of conns already
-				new := newList[i]
-				match := false
-
-				for j := range Conns {
-
-					if new.Name == j {
-						match = true
-						break
-					}
-				}
-
-				//if it can't be found, create a websocket and add it to the map
-				if !match {
-					dialer := &websocket.Dialer{}
-					address := "ws://"
-					address += new.Address
-					address += "/ws"
-					ws, resp, err := dialer.DialContext(context.TODO(), address, nil)
-					if err != nil {
-						log.L.Fatalf("unable to open websocket: %s", err)
-					}
-					defer resp.Body.Close()
-
-					bytes, err := ioutil.ReadAll(resp.Body)
-					if err != nil {
-						log.L.Fatalf("unable to read bytes: %s", bytes)
-					}
-
-					log.L.Debugf("response from opening websocket: %s", bytes)
-					Conns[new.Name] = ws
-					err = ws.WriteMessage(websocket.BinaryMessage, []byte(`{"callBackId":4,"data":{"action":"SetCurrentPage","state":"{\"Page\":\"roomModifyDevices\"}","controller":"App"}}`))
-					if err != nil {
-						log.L.Debugf("unable to read message: %s", err)
-					}
-
-					go ReadMessage(ws)
-				}
-			}
-		}
-	}
+type key struct {
+	Key     string      `json:"Key"`
+	Content interface{} `json:"Content"`
 }
 
-//ReadMessage will read the messages from each websocket and probably forward it whereever it needs to go
-func ReadMessage(ws *websocket.Conn) {
-	// FWD_ENPOINTS should be a string of all of the forwarding urls separated by commas w/o spaces
-	fwdEndpointsString := os.Getenv("FWD_ENDPOINTS")
-	fwdEndpoints := strings.Split(fwdEndpointsString, ",")
+type deviceUpdateContent struct {
+	ID         string `json:"Id"`
+	Connected  bool   `json:"Connected"`
+	IPAddress  string `json:"IPAddress"`
+	Connecting bool   `json:"Connecting"`
+	CanPing    bool   `json:"CanPing"`
+	Aliases    string `json:"Aliases"`
+}
+
+//ReadMessage will read the messages from each websocket
+func ReadMessage(ws *websocket.Conn, name string) {
 	for {
 		_, bytes, err := ws.ReadMessage()
 		if err != nil {
@@ -122,16 +44,84 @@ func ReadMessage(ws *websocket.Conn) {
 
 		str := string(bytes)
 		str = strings.TrimSpace(str)
-		log.L.Debugf("Here is the response: %s", str)
 
-		//now we need to determine what messages we care about and how to send them on
-		//post to smee using the /event endpoint
-		for _, i := range fwdEndpoints {
-			response, err := http.NewRequest("POST", i, strings.NewReader(str))
+		time := time.Now()
+
+		switch {
+		//these are other events that get sent but we don't use them
+		case strings.Contains(str, "\"Key\":\"KeepAliveSocket\""):
+		case strings.Contains(str, "\"Key\":\"Store\""):
+		case strings.Contains(str, "\"Key\":\"DeviceUpdate\""):
+			var k key
+			k.Content = deviceUpdateContent{}
+			err = json.Unmarshal([]byte(str), &k)
 			if err != nil {
-				log.L.Debugf("There was an error forwarding the event to Smee: %s", err)
+				log.L.Debugf("There was an error unmarshaling the json: %s", err)
+				continue
 			}
-			log.L.Debugf("Response from posting to Smee: %s", response)
+			content := k.Content.(deviceUpdateContent)
+			hostnameSearch, err := net.LookupAddr(content.IPAddress)
+			if err != nil {
+				log.L.Debugf("There was an error retrieving the hostname: %s", err)
+			}
+			hostname := strings.Trim(hostnameSearch[0], ".byu.edu.")
+			info := strings.Split(hostname, "-")
+			roomInfo := events.BasicRoomInfo{
+				BuildingID: info[0],
+				RoomID:     info[1],
+			}
+			deviceInfo := events.BasicDeviceInfo{
+				BasicRoomInfo: roomInfo,
+				DeviceID:      hostname,
+			}
+			log.L.Debugf("Here is the response: %s", str)
+			if content.Connected == true {
+				connectedEvent := events.Event{
+					Timestamp:        time,
+					GeneratingSystem: name,
+					TargetDevice:     deviceInfo,
+					Key:              "online",
+					Value:            "Online",
+				}
+				forwardEvent(connectedEvent)
+			} else if content.Connected == false {
+				connectedEvent := events.Event{
+					Timestamp:        time,
+					GeneratingSystem: name,
+					TargetDevice:     deviceInfo,
+					Key:              "online",
+					Value:            "Offline",
+				}
+				forwardEvent(connectedEvent)
+			}
+			addressEvent := events.Event{
+				Timestamp:        time,
+				GeneratingSystem: name,
+				TargetDevice:     deviceInfo,
+				Key:              "ip-address",
+				Value:            content.IPAddress,
+			}
+			forwardEvent(addressEvent)
 		}
+	}
+}
+
+func forwardEvent(e events.Event) {
+	// FWD_ENPOINTS should be a string of all of the forwarding urls separated by commas w/o spaces
+	fwdEndpointsString := os.Getenv("FWD_ENDPOINTS")
+	fwdEndpoints := strings.Split(fwdEndpointsString, ",")
+
+	// now we need to determine what messages we care about and how to send them on
+	// post to smee using the /event endpoint
+	for _, i := range fwdEndpoints {
+		derek, err := json.Marshal(e)
+		if err != nil {
+			log.L.Debugf("There was an error marshaling the json: %s", err)
+		}
+		response, err := http.NewRequest("POST", i, bytes.NewReader(derek))
+		if err != nil {
+			log.L.Debugf("There was an error forwarding the event to %s: %s", i, err)
+		}
+		log.L.Debugf("Response from posting to %s: %s", i, response)
 	}
 }
--- server.go
+++ server.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"io/ioutil"
 	"os"
+	"time"
 
-	"github.com/byuoitav/atlona-event-ms/connection"
-	"github.com/byuoitav/common"
+	"github.com/byuoitav/atlona-event-forwarder/connection"
 	"github.com/gorilla/websocket"
 
 	"github.com/byuoitav/common/db/couch"
 	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/structs"
 )
 
 var (
 	address  = os.Getenv("DB_ADDRESS")
 	username = os.Getenv("DB_USERNAME")
 	password = os.Getenv("DB_PASSWORD")
+	Conns    map[string]*websocket.Conn
 )
 
 func init() {
@@ -27,33 +30,120 @@ func init() {
 func main() {
 	log.SetLevel("debug")
 
-	router := common.NewRouter()
-
-	port := ":10000"
-
 	db := couch.NewDB(address, username, password)
 	agwList, err := db.GetDevicesByType("AtlonaGateway")
-	log.L.Debugf("This is the length of agwlist: %d", len(agwList))
+	// log.L.Debugf("This is the length of agwlist: %d", len(agwList))
 
 	if err != nil {
 		log.L.Fatalf("There was an error getting the AGWList: %v", err)
 	}
+	// fmt.Printf("Length of list and contents: %d %s\n", len(agwList), agwList[0].Address)
 
-	go connection.MonitorAGWList(agwList)
+	Conns = make(map[string]*websocket.Conn)
+	for _, i := range agwList {
+		dialer := &websocket.Dialer{}
+		address := "ws://"
+		address += i.Address
+		address += "/ws"
+		ws, resp, err := dialer.DialContext(context.TODO(), address, nil)
+		if err != nil {
+			log.L.Fatalf("unable to open websocket: %s", err)
+		}
+		defer resp.Body.Close()
+
+		bytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.L.Fatalf("unable to read bytes: %s", bytes)
+		}
+
+		log.L.Debugf("response from opening websocket: %s", bytes)
+		Conns[i.Name] = ws
+	}
 
 	//subscribe
-	for i := range connection.Conns {
-		err = connection.Conns[i].WriteMessage(websocket.BinaryMessage, []byte(`{"callBackId":4,"data":{"action":"SetCurrentPage","state":"{\"Page\":\"roomModifyDevices\"}","controller":"App"}}`))
+	for i := range Conns {
+		err = Conns[i].WriteMessage(websocket.BinaryMessage, []byte(`{"callBackId":4,"data":{"action":"SetCurrentPage","state":"{\"Page\":\"roomModifyDevices\"}","controller":"App"}}`))
 		if err != nil {
 			log.L.Debugf("unable to read message: %s", err)
 		}
 
-		go connection.ReadMessage(connection.Conns[i])
+		go connection.ReadMessage(Conns[i], i)
 	}
 
-	server := http.Server{
-		Addr:           port,
-		MaxHeaderBytes: 1024 * 10,
+	for {
+		//wait some time
+		log.L.Debugf("Waiting to check for list changes")
+		time.Sleep(5 * time.Minute)
+
+		log.L.Debugf("Checking AGWList for changes")
+
+		db := couch.NewDB(address, username, password)
+
+		newAGWList, err := db.GetDevicesByType("AtlonaGateway")
+		if err != nil {
+			log.L.Debugf("there was an issue getting the AGWList: %v", err)
+		}
+
+		//check to see if the length is different
+		if len(Conns) < len(newAGWList) {
+			newList := make([]structs.Device, len(agwList))
+			copy(newList, agwList)
+			log.L.Debugf("comparing the list with the map to find the new one")
+
+			for i := 0; i < len(newList); i++ {
+				//for each object in newList check to see if it exists in the map of conns already
+				new := newList[i]
+				match := false
+
+				for j := range Conns {
+
+					if new.Name == j {
+						match = true
+						break
+					}
+				}
+
+				//if it can't be found, create a websocket and add it to the map
+				if !match {
+					dialer := &websocket.Dialer{}
+					address := "ws://"
+					address += new.Address
+					address += "/ws"
+					ws, resp, err := dialer.DialContext(context.TODO(), address, nil)
+					if err != nil {
+						log.L.Fatalf("unable to open websocket: %s", err)
+					}
+					defer resp.Body.Close()
+
+					bytes, err := ioutil.ReadAll(resp.Body)
+					if err != nil {
+						log.L.Fatalf("unable to read bytes: %s", bytes)
+					}
+
+					log.L.Debugf("response from opening websocket: %s", bytes)
+					Conns[new.Name] = ws
+					err = ws.WriteMessage(websocket.BinaryMessage, []byte(`{"callBackId":4,"data":{"action":"SetCurrentPage","state":"{\"Page\":\"roomModifyDevices\"}","controller":"App"}}`))
+					if err != nil {
+						log.L.Debugf("unable to read message: %s", err)
+					}
+
+					go connection.ReadMessage(ws, new.Name)
+				}
+			}
+		} else if len(Conns) > len(newAGWList) {
+			var found bool
+			for i := range Conns {
+				found = false
+				for _, x := range newAGWList {
+					if i == x.Name {
+						found = true
+						break
+					}
+				}
+				if found == false {
+					delete(Conns, i)
+				}
+			}
+		}
 	}
-	router.StartServer(&server)
 }
